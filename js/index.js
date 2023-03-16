d2dade76610c50697c31aeb85a3994e76a0be5fc
Natalia Balashova <88karamelka88@mail.ru>
2023-03-17 00:39:24 +0300

    work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 const btn = document.getElementById('btn');
 const power = document.getElementById('power');
 const volume = document.getElementById('volume');
+const brand = document.querySelector('.brand');
+const model = document.querySelector('.model');
+const fuel = document.querySelector('.fuelAll');
+const state = document.querySelector('.stateAll');
+const owners = document.querySelector('.ownersAll');
+const age = document.querySelector('.ageAll');
+const payment = document.querySelector('.paymentAll');
+
+//console.log(fuel)
+
+document.getElementById('btn').onclick = function() {
+var value = document.querySelector('.model__select').selectedOptions[0].value;
+document.getElementById("text-price").innerHTML = value;
+console.log(value);
+}
