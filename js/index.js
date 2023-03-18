656d57c5cce12c712b519a462dfd3c3af40e3869
Natalia Balashova <88karamelka88@mail.ru>
2023-03-18 22:51:30 +0300

    js

--- js/index.js
+++ js/index.js
@@ -34,31 +34,63 @@ const jaguar = {'E-Pace' : 'E-Pace',
 /*
 //получение value марок
 // 1 вариант
-  brand.addEventListener('change', function() {
+brand.addEventListener('change', function() {
     const n = this.value;
     //console.log(n)
-  })*/
+})*/
   // 2 вариант
 brand.onchange = function(){
     let item = brand.value;
     //console.log(item)
 }
-  
+
  // получение текста из option
- brand.addEventListener('change', changeBrand);
- function changeBrand(){
+brand.addEventListener('change', changeBrand);
+function changeBrand(){
     let index = brand.selectedIndex; //нашли все индексы наших option
     let item1 = brand.options; //список всех наших option
     let brandNew = item1[index].textContent;
-    console.log(index)
-    console.log(item1)
+    //console.log(index)
+    //console.log(item1)
     //document.getElementById("text-price").innerHTML = brandNew;
     console.log(brandNew);
+    if(item1[1].selected === true){
+        for(let id in opel){
+            const option = document.createElement('option');
+            option.value = id;
+            option.text = opel[id];
+            document.querySelector('.model__select').append(option);
+        }
+    }
+    if(item1[2].selected === true){
+        for(let id in reno){
+            const option = document.createElement('option');
+            option.value = id;
+            option.text = reno[id];
+            document.querySelector('.model__select').append(option);
+        }
+    }
+    if(item1[3].selected === true){
+        for(let id in mazda){
+            const option = document.createElement('option');
+            option.value = id;
+            option.text = mazda[id];
+            document.querySelector('.model__select').append(option);
+        }
+    }
+    if(item1[4].selected === true){
+        for(let id in jaguar){
+            const option = document.createElement('option');
+            option.value = id;
+            option.text = jaguar[id];
+            document.querySelector('.model__select').append(option);
+        }
+    }
 }
 
 
 // создание объекта и из него селект
-for(let id in opel){
+/*for(let id in opel){
     const option = document.createElement('option');
     option.value = id;
     option.text = opel[id];
@@ -81,11 +113,11 @@ for(let id in jaguar){
     option.value = id;
     option.text = jaguar[id];
     //document.querySelector('.model__select').append(option);
-}
+}*/
 
 
 document.getElementById('btn').onclick = function() {
-var value = document.querySelector('.brand__select').selectedOptions[0].value;
+let value = document.querySelector('.brand__select').selectedOptions[0].value;
 document.getElementById("text-price").innerHTML = value;
 console.log(value);
 }
